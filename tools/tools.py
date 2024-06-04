7041f9e175f8e1b0f84275798effcd8822cebd0e
Andres Zelcer <45225345+azelcer@users.noreply.github.com>
2024-06-04 11:18:15 -0300

    Return non-dict

--- tools/tools.py
+++ tools/tools.py
@@ -144,11 +144,11 @@ def saveConfig(main, dateandtime, name, filename=None):
     with open(filename + '.txt', 'w') as configfile:
         config.write(configfile)
 
-def loadConfig(filename):
+def loadConfig(filename) -> configparser.SectionProxy:
     """Load a config file and return just the parameters."""
     config = configparser.ConfigParser()
     config.read(filename)
-    return dict(config['Scanning parameters'])
+    return config['Scanning parameters']
 
 
 def getUniqueName(name):
