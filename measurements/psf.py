ad841966e10d5b5a5fffbc0e0bf63d769755c540
Andres Zelcer <45225345+azelcer@users.noreply.github.com>
2024-07-19 09:49:00 -0300

    Give some time to shutter

--- measurements/psf.py
+++ measurements/psf.py
@@ -508,6 +508,7 @@ class Backend(QtCore.QObject):
                 if self.scan_flag:
                     if not self.alignMode:
                         self.shutterSignal.emit(shutternum, True)
+                        time.sleep(0.100)  # let the shutter move
                     initialPos = np.array([self.target_x, self.target_y,
                                            self.target_z], dtype=np.float64)
                     self.scanSignal.emit(True, 'frame', initialPos)
