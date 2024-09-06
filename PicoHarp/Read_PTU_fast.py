Fix PSF averaging and use true bin numbers

PicoHarp/Read_PTU_fast.py
@@ -238,7 +238,7 @@ def record2time(data: np.ndarray, out_dtime, out_truesync, oflcorrection):
 @numba.njit((numba.uint64[:])(numba.uint32[:], numba.uint64[:], numba.int64),
             parallel=True, boundscheck=True)
 def all_in_one(data, out, binwidth):
-    binwidth = 400
+    # binwidth = 400
     # nbins = 4
     out[:] = 0  # np.zeros((nbins,), dtype=np.uint64)
     for d in data:

measurements/psf.py
@@ -550,9 +550,9 @@ class Backend(QtCore.QObject):
         _lgr.info("Guardando array de PSFs")
         try:
             npx = self.data.shape[-1]  # shady
-            # esto suma de a 4 juntos, si cambiamos el excaneo hay que hacer
+            # esto suma de a 4 juntos, si cambiamos el escaneo hay que hacer
             # o la suma en axis=0 u otro reshape
-            to_save = np.mean(self.data.reshape((-1, 4, npx, npx)), axis=1)
+            to_save = np.mean(self.data.reshape((4, -1, npx, npx)), axis=1)
             _lgr.debug("data shape: %s", to_save.shape)
             np.save(filename + ".npy", to_save)
         except Exception as e: